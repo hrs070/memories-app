74262d92e569b4885ba101a3f9a798854789d238
hrs070 <harshrajsingh97@gmail.com>
2021-08-30 17:20:45 +0530

    Make postModel, createPosts, getPosts route controllers. Only server side changes

--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRouter from './routes/postRoutes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRouter);
 
 const PORT = process.env.PORT || 5000;
 

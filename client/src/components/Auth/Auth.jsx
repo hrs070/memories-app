513fb91fc71f5575ce42d3cee13b2512d2eee8c8
hrs070 <harshrajsingh97@gmail.com>
2021-09-04 01:32:18 +0530

    Google auth and User Id both work in deployed app

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -49,8 +49,9 @@ export default function Auth() {
         }
     }
 
-    function googleFailure() {
+    function googleFailure(error) {
         console.log('Google Sign In failed');
+        console.log(error);
     }
 
     return (
